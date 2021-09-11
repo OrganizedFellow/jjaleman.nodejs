7df84aa4c53d8866aa7670e8094c521c5431bae4
Jaime Aleman <organizedfellow@gmail.com>
2021-09-10 22:55:09 -0600

    added const require variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
+
+// NOT SURE IF THIS CYNTAX IS BETTER, AS THIS IS ES6 COMPLIANT
+// import autoprefixer from 'autoprefixer';
+// import tailwindcss from 'tailwindcss';
+
+
+
 module.exports = {
     plugins: {
         tailwindcss: {},
