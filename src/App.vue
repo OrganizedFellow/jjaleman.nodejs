3a1d50759a2becd570965e57da87d193e412a38d
Jaime Aleman <organizedfellow@gmail.com>
2021-10-07 10:08:41 -0600

    removed contact form

--- src/App.vue
+++ src/App.vue
@@ -64,49 +64,6 @@
             </p>
         </section>
 
-        <section class="py-6 sm:py-8 lg:py-12">
-            <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-                <div class="mb-10 md:mb-16">
-                    <h2 class="text-gray-200 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Let's chat!</h2>
-                    <p class="max-w-screen-md text-gray-200 md:text-lg text-center mx-auto">
-                        I would like to get to know more about your needs.
-                    </p>
-                    <p class="max-w-screen-md text-gray-200 md:text-lg text-center mx-auto">
-                        Send me a quick email and I will get back to you soon.
-                    </p>
-                </div>
-                <form class="max-w-screen-md grid sm:grid-cols-2 gap-4 mx-auto">
-                    <div>
-                        <label for="first-name" class="inline-block text-gray-200 text-sm sm:text-base mb-2">First name*</label>
-                        <input name="first-name" class="w-full bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"/>
-                    </div>
-                    <div>
-                        <label for="last-name" class="inline-block text-gray-200 text-sm sm:text-base mb-2">Last name*</label>
-                        <input name="last-name" class="w-full bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"/>
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="company" class="inline-block text-gray-200 text-sm sm:text-base mb-2">Company</label>
-                        <input name="company" class="w-full bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"/>
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="email" class="inline-block text-gray-200 text-sm sm:text-base mb-2">Email*</label>
-                        <input name="email" class="w-full bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"/>
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="subject" class="inline-block text-gray-200 text-sm sm:text-base mb-2">Subject*</label>
-                        <input name="subject" class="w-full bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"/>
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="message" class="inline-block text-gray-200 text-sm sm:text-base mb-2">Message*</label>
-                        <textarea name="message" class="w-full h-64 bg-gray-50 text-gray-200 border focus:ring ring-blue-300 rounded outline-none transition duration-100 px-3 py-2"></textarea>
-                    </div>
-                    <div class="sm:col-span-2 flex justify-between items-center">
-                        <a href="#" class="inline-block bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus-visible:ring ring-blue-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Send</a>
-                        <span class="text-gray-200 text-sm">*Required</span>
-                    </div>
-                </form>
-            </div>
-        </section>
 
     </div>
     <!-- container -->
